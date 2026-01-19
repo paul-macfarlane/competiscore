64caa21217f01fcc4e71f21b4108a5e697f85847
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2026-01-18 23:16:19 -0500

    chore: swap turso for neon + formatting (#7)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,11 @@
-import { config } from "dotenv";
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: ".env" });
-
 export default defineConfig({
+  out: "./drizzle",
   schema: "./src/db/schema.ts",
-  out: "./migrations",
-  dialect: "turso",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: process.env.DATABASE_URL!,
   },
 });
